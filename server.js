3b583d712044e326163f18ae079aa0996b42c833
Rubens Benoit <rubensbenoit@innoetech.com>
2024-12-13 21:24:37 -0500

    added website at the root server

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(express.json());
 
